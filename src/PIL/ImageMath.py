bca6894bc1a3a0947c8b1bd136af83da0ee3eca3
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2023-12-30 10:23:50 +1100

    Restrict environment keys for ImageMath.eval()
    Prevent environment keys matching the names of builtins and keys
    containing double underscores from being passed to ImageMath.eval(),
    by raising a ValueError.
    
    This is a cherry-pick of python-pillow/Pillow#7655 to fix
    CVE-2023-50447 in Pillow-SIMD. Pillow only fixed it in 10.x, but
    Pillow-SIMD doesn't have that release yet so it's vulnerable. This
    commit solves that issue.

--- src/PIL/ImageMath.py
+++ src/PIL/ImageMath.py
@@ -237,6 +237,11 @@ def eval(expression, _dict={}, **kw):
 
     # build execution namespace
     args = ops.copy()
+    for k in list(_dict.keys()) + list(kw.keys()):
+        if "__" in k or hasattr(builtins, k):
+            msg = f"'{k}' not allowed"
+            raise ValueError(msg)
+
     args.update(_dict)
     args.update(kw)
     for k, v in list(args.items()):
